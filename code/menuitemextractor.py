b9fa1961ef2a26a13beea80f034d1c5fa090709f
cpcluett50 <cpcluett@syr.edu>
2024-12-12 23:03:10 -0500

    these need toi change for my code to run. I think they removed 4 items since the creation of the assignment.

--- code/menuitemextractor.py
+++ code/menuitemextractor.py
@@ -1,9 +1,8 @@
-if __name__ == "__main__":
-    import sys
-    sys.path.append('code')
-    from menuitem import MenuItem
-else:
-    from code.menuitem import MenuItem
+
+import sys
+sys.path.append('code')
+from menuitem import MenuItem
+
 
 
 def clean_price(price:str) -> float:
